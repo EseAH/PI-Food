4e37371fcf980f86346d226fe9f5f05c2a422978
EseAH <ese.aquino@gmail.com>
2022-10-24 18:23:14 -0300

    models db

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -1,12 +1,39 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    summary: {
+      type: DataTypes.TEXT,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 1,
+        max: 10,
+      }
+    },
+    steps: {
+      type: DataTypes.ARRAY(DataTypes.TEXT),
+    },
+    image: {
+      type: DataTypes.TEXT,
+    },
+    createInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  }, {timestamps: false});
 };
